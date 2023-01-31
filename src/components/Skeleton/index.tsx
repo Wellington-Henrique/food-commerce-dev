df5e70f9752b7f951b046f15c88844cc766a74e8
Wellington Henrique <wellingtonhlc@gmail.com>
2023-01-30 21:46:46 -0300

    Implementação de consulta de dados

---
+++ src/components/Skeleton/index.tsx
@@ -0,0 +1,13 @@
+import { SkeletonElement } from './styles';
+
+export interface SkeletonProps {
+    type: 'title' | 'text' | 'image' | 'thumbnail'
+}
+
+const Skeleton = ({type}: SkeletonProps) => {
+  return (
+    <SkeletonElement type={type}/>
+  )
+}
+
+export default Skeleton;
--- src/pages/Burgers/index.tsx
+++ src/pages/Burgers/index.tsx
@@ -1,37 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from '../../components/Head';
 import Snacks from '../../components/Snacks';
 import SnackTitle from '../../components/SnackTitle';
+import { getBurgers } from '../../services/api';
 
 import './styles.tsx';
 
 const BurgersPage = () => {
-  const data = [
-    {
-      id: 1,
-      snack: 'burger',
-      name: 'Mega',
-      description:
-        'O artesanal tamanho família recheado com três carnes suculentas, queijo e bacon.',
-      price: 25.5,
-      image: 'https://i.imgur.com/upjIUnG.jpg',
-    },
-    {
-      id: 2,
-      snack: 'burger',
-      name: 'Extra bacon',
-      description:
-        'Criado para os amantes de bacon, possui em todas as suas camadas bacon bem assado e ainda queijo e carne.',
-      price: 23.5,
-      image: 'https://i.imgur.com/B4J04AJ.jpg',
-    },
-  ]
+  const [burgers, setBurgers] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const burgerRequest = await getBurgers()
+      setBurgers(burgerRequest.data);
+    })()
+  }, []);
 
   return (
       <React.Fragment>
         <Head title='Hámburgueres' description='Nossos melhores burguers'/>
         <SnackTitle>Hambúrgueres</SnackTitle>
-        <Snacks snacks={data}/>
+        <Snacks snacks={burgers}/>
       </React.Fragment>
     )
 }
