5a11dbb7226fe07df40fec0317a1d4dffbd070ea
Wellington Henrique <wellingtonhlc@gmail.com>
2023-03-30 22:39:16 -0300

    Implementação de incremento e decremento de snack

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -73,7 +73,25 @@ export function CartProvider({children} : CarProviderProps ) {
     }
 
     function updateSnackQuantity(snack: Snack, newQuantity: number) :  void {
-        return;
+        if (newQuantity <= 0) return;
+
+        const snackExistentInCart = cart.find(
+            item => item.snack === snack.snack && item.id === snack.id
+        );
+
+        if (!snackExistentInCart) return;
+        
+        const newCart = cart.map(
+            item => {
+                if (item.id === snack.id && item.snack === snackExistentInCart.snack) {
+                    return { ...item, quantity: newQuantity, subtotal: item.price * newQuantity}
+                }
+
+                return item;
+            }
+        );
+
+        setCart(newCart);
     }
 
     function removeSnackFromCart(snack: Snack) :  void {
