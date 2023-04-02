769633cc2f58cac108236baf773232ba4157fc64
Wellington Henrique <wellingtonhlc@gmail.com>
2023-04-01 21:30:35 -0300

    Implementação de renderização condicional de tabela

---
+++ src/pages/MyCart/Table/TableMobile/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+const TableMobile = () => {
+  return (
+    <Container>
+        TableMobile
+    </Container>
+  )
+}
+
+export default TableMobile;
--- src/pages/MyCart/Table/index.tsx
+++ src/pages/MyCart/Table/index.tsx
@@ -1,16 +1,35 @@
+import { useEffect, useState } from "react";
 import { useCart } from "../../../hooks/useCart";
 
 import TableDesktop, { } from './TableDesktop';
+import TableMobile from "./TableMobile";
 
 const Table = () => {
+    const [ windowWidth, setWindowWidth] = useState(document.documentElement.clientWidth);
     const { cart } = useCart();
+
+    useEffect(() => {
+        function updateTableComponentBasedInWindowWidth () {
+            const currentWidth = document.documentElement.clientWidth;
+
+            setWindowWidth(currentWidth);
+        }
+
+        window.addEventListener('resize', updateTableComponentBasedInWindowWidth)
+        
+        return () => {
+            window.removeEventListener('resize', updateTableComponentBasedInWindowWidth)
+        }
+    }, [])
 
     if (!cart.length) {
         return <h1>Ops! parece que você não tem pedidos, peça já!</h1>;
     }
 
     return (
+        windowWidth > 768 ?
         <TableDesktop/>
+        : <TableMobile/>
     )
 }
 
