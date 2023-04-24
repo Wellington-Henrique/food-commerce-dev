2fa1ad2c8b9c339fb9e1f52670e9dc28c7a187ce
Wellington Henrique <wellingtonhlc@gmail.com>
2023-04-24 20:09:38 -0300

    Validação de CPF e endereço

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -7,191 +7,258 @@ import Head from "../../components/Head";
 import PayOrder from "../../components/OrderCloseAction/PayOrder";
 import OrderHeader from "../../components/OrderHeader";
 
-import { FieldValues, schema } from './validationSchema';
+import { FieldValues, schema } from './validationSchema'
 
-import { Container, Form, Inner } from "./styles";
+import { Container, Form, Inner } from './styles'
 
-const Payment = () => {
+export default function Payment() {
   const {
     control,
     handleSubmit,
-    formState: { errors }
+    formState: { errors },
   } = useForm<FieldValues>({
-    resolver: yupResolver(schema)
-  });
-
-  const onSubmit: SubmitHandler<FieldValues> = (data) => { console.log(data) }
+    resolver: yupResolver(schema),
+  })
+  const onSubmit: SubmitHandler<FieldValues> = (data) => console.log('data', data)
 
   return (
     <Container>
-      <Head title="Pagamento"/>
-      <OrderHeader/>
+      <Head title='Pagamento' />
+      <OrderHeader />
       <Inner>
         <Form onSubmit={handleSubmit(onSubmit)}>
           <h4>Informações pessoais</h4>
 
-          <div className="field">
-            <label htmlFor="fullName">Nome e sobrenome</label>
+          <div className='field'>
+            <label htmlFor='fullName'>Nome e sobrenome</label>
+
             <Controller
               name='fullName'
               control={control}
-              render={({ field }) =>
-                <input
-                type="text"
-                id="fullName"
-                autoComplete="name"
-                { ...field}
-              />
-            }/>
+              render={({ field }) => (
+                <input type='text' id='fullName' autoComplete='name' {...field} />
+              )}
+            />
 
-            {errors.fullName &&
-            <p className='error'>{ errors.fullName.message }</p>}
+            {errors.fullName && <p className='error'>{errors.fullName.message}</p>}
           </div>
 
-          <div className="grouped">
-            <div className="field">
-              <label htmlFor="email">E-mail</label>
+          <div className='grouped'>
+            <div className='field'>
+              <label htmlFor='email'>E-mail</label>
+
               <Controller
                 name='email'
                 control={control}
-                render={({ field }) =>
-                  <input
-                    type="text"
-                    id="email"
-                    autoComplete="email"
-                    { ...field}
-                  />
-              }/>
-              {errors.email &&
-              <p className='error'>{ errors.email.message }</p>}
+                render={({ field }) => (
+                  <input type='email' id='email' autoComplete='email' {...field} />
+                )}
+              />
+
+              {errors.email && <p className='error'>{errors.email.message}</p>}
             </div>
 
-            <div className="field">
-              <label htmlFor="mobile">Celular</label>
+            <div className='field'>
+              <label htmlFor='mobile'>Celular</label>
               <Controller
                 name='mobile'
                 control={control}
-                render={({ field }) =>
+                render={({ field }) => (
                   <IMaskInput
-                    type="tel"
-                    id="mobile"
-                    autoComplete="phone"
+                    type='tel'
+                    id='mobile'
+                    autoComplete='phone'
                     mask={'(00) 90000-0000'}
-                    { ...field}
+                    {...field}
                   />
-              }/>
-              {errors.mobile &&
-            <p className='error'>{ errors.mobile.message }</p>}
+                )}
+              />
+              {errors.mobile && <p className='error'>{errors.mobile.message}</p>}
             </div>
 
-            <div className="field">
-              <label htmlFor="document">CPF / CNPJ</label>
-              <input type="text" id="document" name="document"/>
+            <div className='field'>
+              <label htmlFor='document'>CPF/CNPJ</label>
+              <Controller
+                name='document'
+                control={control}
+                render={({ field }) => (
+                  <IMaskInput
+                    type='text'
+                    id='document'
+                    mask={[
+                      { mask: '000.000.000-00', maxLength: 11 },
+                      { mask: '00.000.000/0000-00' },
+                    ]}
+                    {...field}
+                  />
+                )}
+              />
+              {errors.document && <p className='error'>{errors.document.message}</p>}
             </div>
           </div>
 
           <h4>Endereço de entrega</h4>
 
-          <div className="field">
-            <label htmlFor="zipcode">CEP</label>
-            <input
-              type="text"
-              id="zipcode"
-              name="zipcode"
-              autoComplete="postal-code"
-              style={{width: '120px'}}
+          <div className='field'>
+            <label htmlFor='zipCode'>CEP</label>
+            <Controller
+              name='zipCode'
+              control={control}
+              render={({ field }) => (
+                <IMaskInput
+                  type='text'
+                  id='zipCode'
+                  style={{ width: '120px' }}
+                  mask={'00000-000'}
+                  {...field}
+                />
+              )}
             />
-          </div>
-
-          <div className="field">
-            <label htmlFor="street">Endereço</label>
-            <input
-              type="text"
-              id="street"
-              name="street"
-              autoComplete="street-address"
-            />
-          </div>
-
-          <div className="grouped">
-            <div className="field">
-              <label htmlFor="number">Número</label>
-              <input type="text" id="number" name="number"/>
-            </div>
-
-            <div className="field">
-              <label htmlFor="complement">Complemento</label>
-              <input type="text" id="complement" name="complement"/>
-            </div>
-
-            <div className="field">
-              <label htmlFor="document">CPF / CNPJ</label>
-              <input type="text" id="document" name="document"/>
-            </div>
+            {errors.zipCode && <p className='error'>{errors.zipCode.message}</p>}
           </div>
 
           <div className='field'>
-            <label htmlFor='state'>Estado</label>
-            <select id='state' name='state'>
-              <option value=''>Selecione</option>
-              <option value='AC'>Acre</option>
-              <option value='AL'>Alagoas</option>
-              <option value='AP'>Amapá</option>
-              <option value='AM'>Amazonas</option>
-              <option value='BA'>Bahia</option>
-              <option value='CE'>Ceará</option>
-              <option value='ES'>Espírito Santo</option>
-              <option value='GO'>Goiás</option>
-              <option value='MA'>Maranhão</option>
-              <option value='MT'>Mato Grosso</option>
-              <option value='MS'>Mato Grosso do Sul</option>
-              <option value='MG'>Minas Gerais</option>
-              <option value='PA'>Pará</option>
-              <option value='PB'>Paraíba</option>
-              <option value='PR'>Paraná</option>
-              <option value='PE'>Pernambuco</option>
-              <option value='PI'>Piauí</option>
-              <option value='RJ'>Rio de Janeiro</option>
-              <option value='RN'>Rio Grande do Norte</option>
-              <option value='RS'>Rio Grande do Sul</option>
-              <option value='RO'>Rondônia</option>
-              <option value='RR'>Roraima</option>
-              <option value='SC'>Santa Catarina</option>
-              <option value='SP'>São Paulo</option>
-              <option value='SE'>Sergipe</option>
-              <option value='TO'>Tocantins</option>
-              <option value='DF'>Distrito Federal</option>
-            </select>
+            <label htmlFor='street'>Endereço</label>
+            <Controller
+              name='street'
+              control={control}
+              render={({ field }) => <input type='text' id='street' {...field} />}
+            />
+            {errors.street && <p className='error'>{errors.street.message}</p>}
+          </div>
+
+          <div className='grouped'>
+            <div className='field'>
+              <label htmlFor='number'>Número</label>
+              <Controller
+                name='number'
+                control={control}
+                render={({ field }) => <input type='text' id='number' {...field} />}
+              />
+              {errors.number && <p className='error'>{errors.number.message}</p>}
+            </div>
+
+            <div className='field'>
+              <label htmlFor='complement'>Complemento</label>
+              <Controller
+                name='complement'
+                control={control}
+                render={({ field }) => <input type='text' id='complement' {...field} />}
+              />
+              {errors.complement && <p className='error'>{errors.complement.message}</p>}
+            </div>
+          </div>
+
+          <div className='grouped'>
+            <div className='field'>
+              <label htmlFor='neighborhood'>Bairro</label>
+              <Controller
+                name='neighborhood'
+                control={control}
+                render={({ field }) => <input type='text' id='neighborhood' {...field} />}
+              />
+              {errors.neighborhood && <p className='error'>{errors.neighborhood.message}</p>}
+            </div>
+
+            <div className='field'>
+              <label htmlFor='city'>Cidade</label>
+              <Controller
+                name='city'
+                control={control}
+                render={({ field }) => <input type='text' id='city' {...field} />}
+              />
+              {errors.city && <p className='error'>{errors.city.message}</p>}
+            </div>
+
+            <div className='field'>
+              <label htmlFor='state'>Estado</label>
+              <Controller
+                name='state'
+                control={control}
+                render={({ field }) => (
+                  <select id='state' {...field}>
+                    <option value=''>Selecione</option>
+                    <option value='AC'>Acre</option>
+                    <option value='AL'>Alagoas</option>
+                    <option value='AP'>Amapá</option>
+                    <option value='AM'>Amazonas</option>
+                    <option value='BA'>Bahia</option>
+                    <option value='CE'>Ceará</option>
+                    <option value='ES'>Espírito Santo</option>
+                    <option value='GO'>Goiás</option>
+                    <option value='MA'>Maranhão</option>
+                    <option value='MT'>Mato Grosso</option>
+                    <option value='MS'>Mato Grosso do Sul</option>
+                    <option value='MG'>Minas Gerais</option>
+                    <option value='PA'>Pará</option>
+                    <option value='PB'>Paraíba</option>
+                    <option value='PR'>Paraná</option>
+                    <option value='PE'>Pernambuco</option>
+                    <option value='PI'>Piauí</option>
+                    <option value='RJ'>Rio de Janeiro</option>
+                    <option value='RN'>Rio Grande do Norte</option>
+                    <option value='RS'>Rio Grande do Sul</option>
+                    <option value='RO'>Rondônia</option>
+                    <option value='RR'>Roraima</option>
+                    <option value='SC'>Santa Catarina</option>
+                    <option value='SP'>São Paulo</option>
+                    <option value='SE'>Sergipe</option>
+                    <option value='TO'>Tocantins</option>
+                    <option value='DF'>Distrito Federal</option>
+                  </select>
+                )}
+              />
+              {errors.state && <p className='error'>{errors.state.message}</p>}
+            </div>
           </div>
 
           <h4>Pagamento</h4>
 
-          <div className="field">
-            <label htmlFor="credit-cart-number">Número do cartão</label>
-            <input type="text" id="credit-cart-number" name="credit-cart-number" autoComplete="cc-number"/>
+          <div className='field'>
+            <label htmlFor='credit-card-number'>Número do cartão</label>
+            <input
+              type='text'
+              id='credit-card-number'
+              name='credit-card-number'
+              autoComplete='cc-number'
+            />
           </div>
 
-          <div className="field">
-            <label htmlFor="credit-card-hold-name">Nome impresso no cartão</label>
-            <input type="text" id="credit-card-hold-name" name="credit-card-hold-name" autoComplete="cc-name"/>
+          <div className='field'>
+            <label htmlFor='credit-card-holder-name'>Nome impresso no cartão</label>
+            <input
+              type='text'
+              id='credit-card-holder-name'
+              name='credit-card-holder-name'
+              autoComplete='cc-name'
+            />
           </div>
 
-          <div className="grouped">
-            <div className="field">
-              <label htmlFor="credit-card-expiration">Validade (MM/AA)</label>
-              <input type="text" id="credit-card-expiration" name="credit-card-expiration" autoComplete="cc-exp"/>
+          <div className='grouped'>
+            <div className='field'>
+              <label htmlFor='credit-card-expiration'>Validade (MM/AA)</label>
+              <input
+                type='text'
+                id='credit-card-expiration'
+                name='credit-card-expiration'
+                autoComplete='cc-exp'
+              />
             </div>
 
-            <div className="field">
-              <label htmlFor="credit-card-code">Código de segurança (CVV)</label>
-              <input type="text" id="credit-card-code" name="credit-card-code" autoComplete="cc-csc"/>
+            <div className='field'>
+              <label htmlFor='credit-card-code'>Código de segurança (CVV)</label>
+              <input
+                type='text'
+                id='credit-card-code'
+                name='credit-card-code'
+                autoComplete='cc-csc'
+              />
             </div>
           </div>
-          <PayOrder/>
+          <PayOrder />
         </Form>
       </Inner>
     </Container>
   )
 }
 
-export default Payment;
